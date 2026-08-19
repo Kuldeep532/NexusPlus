b8f156756f29d8772d078fa708ff833501b8df91
Kuldeep532 <126377510+Kuldeep532@users.noreply.github.com>
2026-08-20 03:18:11 +0530

    fix: register utilities navigation routes

--- artifacts/nexus-plus/app/_layout.tsx
+++ artifacts/nexus-plus/app/_layout.tsx
@@ -33,9 +33,11 @@ function RootLayoutNav() {
       <Stack.Screen name="media-player" options={{ title: 'Media Player' }} />
       <Stack.Screen name="biometric-vault" options={{ title: 'Biometric Vault' }} />
       <Stack.Screen name="selfie" options={{ title: 'Selfie' }} />
-      <Stack.Screen name="time-announcer" options={{ title: 'Time Announcer' }} />
+      <Stack.Screen name="time-announcer" options={{ title: 'Time Announcer', href: null }} />
       <Stack.Screen name="battery-announcer" options={{ title: 'Battery Announcer' }} />
       <Stack.Screen name="language-and-preference" options={{ title: 'Language and Preference' }} />
+      <Stack.Screen name="utilities" options={{ title: 'Utilities' }} />
+      <Stack.Screen name="utilities/qr-generator" options={{ title: 'QR Code Generator' }} />
       <Stack.Screen name="voices" options={{ title: 'Voice Library' }} />
       <Stack.Screen name="pdf-tools" options={{ title: 'PDF Tools' }} />
       <Stack.Screen name="pdf-to-images" options={{ title: 'PDF to Images' }} />
