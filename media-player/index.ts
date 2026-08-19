64ec4d1a9fdf0ef4cac9faacf8ac754ed3f7b10a
Kuldeep532 <126377510+Kuldeep532@users.noreply.github.com>
2026-08-19 19:32:10 +0530

    feat: export Nexus media module

---
+++ media-player/index.ts
@@ -0,0 +1,6 @@
+export * from './types';
+export * from './subtitles';
+export * from './library';
+export * from './LocalMediaAI';
+export * from './useMediaPlayer';
+export * from './NexusMediaPlayer';
