24701d9b4b51c010a07c4fe98db599510974bc57
Kuldeep532 <126377510+Kuldeep532@users.noreply.github.com>
2026-08-20 03:07:51 +0530

    feat: register Battery Announcer and language preference routes

--- artifacts/nexus-plus/app/_layout.tsx
+++ artifacts/nexus-plus/app/_layout.tsx
@@ -34,6 +34,8 @@ function RootLayoutNav() {
       <Stack.Screen name="biometric-vault" options={{ title: 'Biometric Vault' }} />
       <Stack.Screen name="selfie" options={{ title: 'Selfie' }} />
       <Stack.Screen name="time-announcer" options={{ title: 'Time Announcer' }} />
+      <Stack.Screen name="battery-announcer" options={{ title: 'Battery Announcer' }} />
+      <Stack.Screen name="language-and-preference" options={{ title: 'Language and Preference' }} />
       <Stack.Screen name="voices" options={{ title: 'Voice Library' }} />
       <Stack.Screen name="pdf-tools" options={{ title: 'PDF Tools' }} />
       <Stack.Screen name="pdf-to-images" options={{ title: 'PDF to Images' }} />
