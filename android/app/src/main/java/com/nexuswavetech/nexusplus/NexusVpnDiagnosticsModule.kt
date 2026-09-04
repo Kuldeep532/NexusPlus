6bd8149c41b3f75a5e949d2eaf20a2936ec5baba
Kuldeep532 <126377510+Kuldeep532@users.noreply.github.com>
2026-09-04 21:53:31 +0530

    feat(vpn): include DNS outcomes in diagnostics

--- android/app/src/main/java/com/nexuswavetech/nexusplus/NexusVpnDiagnosticsModule.kt
+++ android/app/src/main/java/com/nexuswavetech/nexusplus/NexusVpnDiagnosticsModule.kt
@@ -1,5 +1,6 @@
 package com.nexuswavetech.nexusplus
 
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -11,13 +12,21 @@ class NexusVpnDiagnosticsModule(reactContext: ReactApplicationContext) : ReactCo
 
     @ReactMethod
     fun getStats(promise: Promise) {
-        runCatching { promise.resolve(NexusVpnPacketStats.snapshot()) }
-            .onFailure { promise.reject("VPN_STATS", it.message ?: "Unable to read VPN diagnostics") }
+        runCatching {
+            val stats = Arguments.createMap().apply {
+                NexusVpnPacketStats.snapshot().forEach { (key, value) -> putDouble(key, value.toDouble()) }
+                NexusVpnDnsStats.snapshot().forEach { (key, value) -> putDouble("dns_$key", value.toDouble()) }
+            }
+            promise.resolve(stats)
+        }.onFailure { promise.reject("VPN_STATS", it.message ?: "Unable to read VPN diagnostics") }
     }
 
     @ReactMethod
     fun resetStats(promise: Promise) {
-        runCatching { NexusVpnPacketStats.reset(); promise.resolve(true) }
-            .onFailure { promise.reject("VPN_STATS_RESET", it.message ?: "Unable to reset VPN diagnostics") }
+        runCatching {
+            NexusVpnPacketStats.reset()
+            NexusVpnDnsStats.reset()
+            promise.resolve(true)
+        }.onFailure { promise.reject("VPN_STATS_RESET", it.message ?: "Unable to reset VPN diagnostics") }
     }
 }
