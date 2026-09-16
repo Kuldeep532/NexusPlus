4e6eb85629f8e8259cceb936e900f1e135deb821
Kuldeep532 <126377510+Kuldeep532@users.noreply.github.com>
2026-09-16 12:59:58 +0530

    feat(android): connect Video Editor to native cutter

--- android/app/src/main/java/com/nexuswavetech/nexusplus/NexusVideoEditorModule.kt
+++ android/app/src/main/java/com/nexuswavetech/nexusplus/NexusVideoEditorModule.kt
@@ -1,5 +1,6 @@
 package com.nexuswavetech.nexusplus
 
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -11,11 +12,26 @@ class NexusVideoEditorModule(private val reactContext: ReactApplicationContext) 
 
     @ReactMethod
     fun isAvailable(promise: Promise) {
-        promise.resolve(false)
+        promise.resolve(true)
     }
 
     @ReactMethod
     fun execute(operation: ReadableMap, promise: Promise) {
-        promise.reject("VIDEO_NATIVE", "Native video engine registration is pending in the Android build.")
+        try {
+            val type = operation.getString("type") ?: throw IllegalArgumentException("Video operation type is required.")
+            when (type) {
+                "remove-segment" -> {
+                    val inputUri = operation.getString("inputUri") ?: throw IllegalArgumentException("Input video URI is required.")
+                    val outputUri = operation.getString("outputUri") ?: throw IllegalArgumentException("Output video URI is required.")
+                    val startMs = operation.getDouble("startMs")
+                    val endMs = operation.getDouble("endMs")
+                    val result = NexusVideoCutter.removeSegment(reactContext, inputUri, outputUri, startMs, endMs)
+                    promise.resolve(Arguments.makeNativeMap(result))
+                }
+                else -> promise.reject("VIDEO_OPERATION_UNSUPPORTED", "Video operation '$type' is not implemented yet.")
+            }
+        } catch (error: Throwable) {
+            promise.reject("VIDEO_NATIVE", error.message ?: "Native video operation failed.", error)
+        }
     }
 }
