3ca667b1db0c1376860b98e91d3f21b58c8bc7ab
Kuldeep532 <126377510+Kuldeep532@users.noreply.github.com>
2026-09-17 11:26:02 +0530

    fix: remove dead vocal remover package registration

--- android/app/src/main/java/com/nexuswavetech/nexusplus/NexusReactApplication.kt
+++ android/app/src/main/java/com/nexuswavetech/nexusplus/NexusReactApplication.kt
@@ -30,7 +30,6 @@ class NexusReactApplication : Application(), ReactApplication {
             add(NexusCctvOnvifPackage())
             add(NexusPdfNativePackage())
             add(NexusVideoEditorPackage())
-            add(NexusVocalRemoverPackage())
         }
         override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
