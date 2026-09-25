ac3b96e1df91f5297e7daef182637a4e58c8cc40
Kuldeep532 <126377510+Kuldeep532@users.noreply.github.com>
2026-09-25 14:24:25 +0530

    production: harden NexusReactApplication.kt

--- android/app/src/main/java/com/nexuswavetech/nexusplus/NexusReactApplication.kt
+++ android/app/src/main/java/com/nexuswavetech/nexusplus/NexusReactApplication.kt
@@ -7,7 +7,6 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
@@ -43,7 +42,7 @@ class NexusReactApplication : Application(), ReactApplication {
 
             override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
             override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isNewArchEnabled: Boolean = false
             override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
         })
 
@@ -52,11 +51,7 @@ class NexusReactApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
-        // Native loader is initialized before any manually registered module can be touched.
         SoLoader.init(this, OpenSourceMergedSoMapping)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            load()
-        }
         runCatching {
             ApplicationLifecycleDispatcher.onApplicationCreate(this)
         }
